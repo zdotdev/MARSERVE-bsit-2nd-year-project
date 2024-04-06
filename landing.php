a01219aa9e680a4995d829388b43014004cebf37
zdotdev <www.ragaszedrick25@gmail.com>
2024-04-07 00:49:24 +0800

    Added design for landing.php

--- landing.php
+++ landing.php
@@ -25,20 +25,27 @@ foreach ($orders->order as $order) {
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="./Style/landing.css">
         <title>Order placed!!!</title>
         <!-- zdotdev -->
     </head>
     <body>
+        <header>
+            <h1>MARSERVE</h1>
+        </header>
         <main>
+            <h1 class='title'>Orders:</h1>
             <?php
                 if ($foundOrder !== null) {
-                    echo "<h1>Order Details</h1>";
                     foreach ($orders->order as $order) {
                         if ((string) $order->table_number === $table) {
                             echo "
-                                <div>
-                                    <p>Total Orders: {$order->total_orders}</p>
-                                    <p>Total Bill: {$order->total_bill}</p>
+                                <div class='each-orders'>
+                                    <p class='order-names'>Total Orders: {$order->total_orders}</p>
+                                    <p class='bill-container'>Price: <span id='bill'>{$order->total_bill}</span></p>
                                 </div>
                             ";
                         }
@@ -47,9 +54,24 @@ foreach ($orders->order as $order) {
                     echo "Order not found.";
                 }
                 echo "
-                <a href='http://localhost/orderSystem/index.php?table={$order->table_number}&orderId={$orderId}'>Buy again?</a>
-                <p>Table Number{$table}</p>";
+                <div class='total-div'>
+                    <p>Table Number: {$table}</p>
+                    <p>Total bill: <span id='total'></span> php</p>
+                </div>";
+                echo "
+                <div class='buy-again-div'>
+                    <a href='http://localhost/orderSystem/index.php?table={$order->table_number}&orderId={$orderId}'>Buy again?</a>
+                </div>";
             ?>
         </main>
+        <script>
+            let arr = [];
+            document.querySelectorAll('#bill').forEach(function (el) {
+                el.textContent.split(' ').forEach(function (item) {
+                    arr.push(parseFloat(item));
+                });
+            });
+            document.getElementById('total').textContent = arr.reduce((total, num) => total + num, 0);
+        </script>
     </body>
 </html>
